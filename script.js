1354369b9133cbb5c43df5097c4c30a401b603db
NereaLosanezmrn <nerea.moreno@bootcamp-upgrade.com>
2021-12-11 21:35:00 +0100

    Iteration#4

--- script.js
+++ script.js
@@ -52,3 +52,21 @@ const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 
 for (let value of placesToTravel) {
     console.log(value);
 }
+
+
+
+//ITERACIÓN #4: PROBANDO FOR...IN
+
+//Usa un **for...in** para imprimir por consola los datos del alienígena. Puedes usar este objeto:
+
+
+const alien = {
+    name: 'Wormuck',
+    race: 'Cucusumusu',
+    planet: 'Eden',
+    weight: '259kg'
+}
+
+for( const prop in alien){
+    console.log(`${prop}: ${alien[prop]}`);
+}
