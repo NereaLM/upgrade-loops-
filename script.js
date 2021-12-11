13035ba772d8778ddc230207c337756ea50a5381
NereaLosanezmrn <nerea.moreno@bootcamp-upgrade.com>
2021-12-11 21:31:48 +0100

    Iteration#2

--- script.js
+++ script.js
@@ -11,3 +11,33 @@ products.forEach( (item) =>{
         console.log(item)
     }
 })
+
+
+//ITERACIÓN #2: CONDICIONALES AVANZADAS
+
+//Comprueba en cada uno de los usuarios que tenga al menos dos trimestres aprobados y añade la propiedad ***isApproved*** a true o false en consecuencia. Una vez lo tengas compruébalo con un ***console.log***
+//Puedes usar este array para probar tu función:
+
+
+const alumns = [
+    {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
+		{name: 'Lucia Aranda', T1: true, T2: false, T3: true},
+		{name: 'Juan Miranda', T1: false, T2: true, T3: true},
+		{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
+		{name: 'Raquel Benito', T1: true, T2: true, T3: true}
+]
+
+let print = (data) => console.log(data)
+
+let isAproved = (data) => {
+    for (let i = 0; i < data.length; i++){
+        if ((data[i].T1 === true && data[i].T2 === true) || (data[i].T2 === true && data[i].T3 === true) || (data[i].T1 === true && data[i].T3 === true)){
+            print (data[i].name + "is aproved")
+        } else {
+            print (data[i].name + "is not aproved")
+        }
+    }
+}
+
+isAproved(alumns)
+
